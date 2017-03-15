40927a184b740311976970c422b686512321f4cc
shuoshuren <534361124@qq.com>
2017-03-15 10:03:26 +0800

    MaterialDesign中的CardView和RecyclerView的使用

--- materialdesign/src/main/java/com/example/xiao/materialdesign/MainActivity.java
+++ materialdesign/src/main/java/com/example/xiao/materialdesign/MainActivity.java
@@ -9,17 +9,36 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.xiao.materialdesign.adapter.FruitAdapter;
+import com.example.xiao.materialdesign.bean.Fruit;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
     private FloatingActionButton mFloatingBtn;
+    private RecyclerView mRecyclerView;
+
+    private FruitAdapter mAdapter;
+    private Fruit[] fruits = {new Fruit("apple",R.drawable.apple),new Fruit("banana",R.drawable.banana),
+            new Fruit("Orange",R.drawable.orange),new Fruit("Watermelon",R.drawable.watermelon),
+            new Fruit("Pear",R.drawable.pear),new Fruit("Grape",R.drawable.grape),
+            new Fruit("PineApple",R.drawable.pineapple),new Fruit("StrawBerry",R.drawable.strawberry),
+            new Fruit("Cherry",R.drawable.cherry),new Fruit("Mango",R.drawable.mango)
+    };
+
+    private List<Fruit> fruitList = new ArrayList<Fruit>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+
+        initData();
         initView();
         initEvent();
 
@@ -44,12 +65,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void initData() {
+        for (Fruit fruit:fruits){
+            fruitList.add(fruit);
+        }
+    }
+
+
     private void initView() {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setCheckedItem(R.id.nav_call);//设置默认选中的Item
+
         mFloatingBtn = (FloatingActionButton) findViewById(R.id.floating);
 
-        mNavigationView.setCheckedItem(R.id.nav_call);//设置默认选中的Item
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mAdapter = new FruitAdapter(this,fruitList);
+        mRecyclerView.setAdapter(mAdapter);
+
+
     }
 
     private void initEvent() {
---
+++ materialdesign/src/main/java/com/example/xiao/materialdesign/adapter/FruitAdapter.java
@@ -0,0 +1,66 @@
+package com.example.xiao.materialdesign.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.xiao.materialdesign.R;
+import com.example.xiao.materialdesign.bean.Fruit;
+
+import java.util.List;
+
+/**
+ * Created by xiao on 2017/3/15.
+ */
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Fruit> mDatas;
+
+    public FruitAdapter(Context context, List<Fruit> datas) {
+        this.mContext = context;
+        this.mDatas = datas;
+    }
+
+    @Override
+    public FruitAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View rootView = LayoutInflater.from(mContext).inflate(R.layout.fruit_item,parent,false);
+
+        return new ViewHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(FruitAdapter.ViewHolder holder, int position) {
+        Fruit fruit = mDatas.get(position);
+        holder.fruitText.setText(fruit.getName());
+        Glide.with(mContext).load(fruit.getImageId()).into(holder.fruitImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cardView;
+        ImageView fruitImage;
+        TextView fruitText;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView;
+            fruitImage = (ImageView) cardView.findViewById(R.id.fruit_image);
+            fruitText = (TextView) cardView.findViewById(R.id.fruit_name);
+        }
+    }
+}
